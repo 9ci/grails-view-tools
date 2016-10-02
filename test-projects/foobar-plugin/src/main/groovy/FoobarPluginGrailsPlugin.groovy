65b76595a43b0a9a2e49ab289bb7ba09ecfacbe3
Joshua Burnett <joshua@greenbill.com>
2016-10-01 21:42:59 -0500

    test-projects upgrade to grails 3

--- test-projects/foobar-plugin/src/main/groovy/FoobarPluginGrailsPlugin.groovy
+++ test-projects/foobar-plugin/src/main/groovy/FoobarPluginGrailsPlugin.groovy
@@ -1,22 +1,25 @@
-class FoobarPluginGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
+
+import grails.plugins.*
+
+class FoobarPluginGrailsPlugin extends Plugin {
+
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.5 > *"
+    def grailsVersion = "3.1.9 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
     ]
 
     // TODO Fill in these fields
-    def title = "View Test Plugin Plugin" // Headline display name of the plugin
+    def title = "G3plugin" // Headline display name of the plugin
     def author = "Your name"
     def authorEmail = ""
     def description = '''\
 Brief summary/description of the plugin.
 '''
+    def profiles = ['web']
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/view-test-plugin"
+    def documentation = "http://grails.org/plugin/g3plugin"
 
 }
