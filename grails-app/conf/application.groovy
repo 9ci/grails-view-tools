843c4e6834d6837c0a34373036462ffba6af5044
sudhir nimavat <sudhir@nimavat.me>
2017-10-02 12:12:10 +0530

    move app resource service to view tools

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,43 @@
+
+nine {
+	resources {
+
+		currentTenant = { return [id:1, num:"testTenant"]}
+		tempDir = {
+			def props = System.properties
+			File tmp = new File("${props['java.io.tmpdir']}/${props['user.name']}/9ci-app/temp")
+			if(!tmp.exists()) tmp.mkdirs() // appResourceService doesn't want to create directories which are not inside rootLocation.
+			return tmp.canonicalPath // In order for appResourceService to allow it to be outside of rootLocation it must be absolute.
+		}
+		attachments.location = 'attachments'
+
+		rootLocation = { args ->
+			// AppResourcesService will not create rootLocation directory or an absolute path.  This is in target, so we need something extra.
+			// For development we want a persistent non-SCM location common to all components but specific to a customer so we can
+			// have customer data or common external files.
+			File file = new File("../rootLocation")
+			if(!file.exists()) {
+				println "Creating rootLocation ${file.canonicalPath} for testing purposes."
+				file.mkdirs()
+			}
+			return file.canonicalPath
+		}
+
+		scripts{
+			// For test we want two script locations:
+			// 1. The primary (first) location needs to be what's checked into SCM.
+			//    * We have tests which examine the first scripts location for files.
+			// 2. The default target/rootLocation/scripts.
+			//    * We have tests which create a script and execute it.
+			locations = { args ->
+				// AppResourcesService will not create an absolute directory.  This is not in rootLocation, so we need something extra.
+				File file = new File('src/integration-test/resources')
+				if(!file.exists()) {
+					println "Creating ${file.canonicalPath} for testing purposes."
+					file.mkdirs()
+				}
+				return [ file.canonicalPath, 'scripts' ]
+			}
+		}
+	}
+}
---
+++ src/integration-test/groovy/grails/plugin/viewtools/AppResourceServiceSpec.groovy
@@ -0,0 +1,295 @@
+package grails.plugin.viewtools
+
+import grails.test.mixin.integration.Integration
+import org.apache.commons.io.FileUtils
+import spock.lang.Shared
+import spock.lang.Specification
+
+@Integration
+class AppResourceServiceSpec extends Specification {
+
+	@Shared AppResourceService appResourceService
+
+	void cleanupSpec() {
+		FileUtils.deleteDirectory(appResourceService.getLocation("attachments.location"))
+	}
+
+	def testCreateAttachmentFile_empty_noCreation() {
+		when:
+		def result = appResourceService.createAttachmentFile(1L, null,'txt', null)
+
+		then:
+		result == null
+	}
+
+	def testCreateAttachmentFile_string() {
+		when:
+		def CONTENT = 'hello, world!'
+		def result = appResourceService.createAttachmentFile(2L, 'xyz','txt', CONTENT)
+
+		then:
+		result
+
+		when:
+		def datePart = new Date().format('yyyy-MM')
+
+		then:
+		result.location.endsWith("${datePart}/xyz_2.txt")
+		result.file
+		//assert result.file.absolutePath == "xxx"
+		'xyz_2.txt' == result.file.name
+		result.location == appResourceService.getRelativePath('attachments.location', result.file)
+		result.file.exists()
+		CONTENT.size() == result.file.size()
+	}
+
+	def testCreateAttachmentFile_bytes() {
+		when:
+		def CONTENT = 'hello, world!'.bytes
+		def result = appResourceService.createAttachmentFile(2L, null,'txt', CONTENT)
+
+		then:
+		result
+		result.location
+		result.location.size() > 3
+		result.location.endsWith(".txt")
+		result.file
+		'2.txt' == result.file.name
+		result.location == appResourceService.getRelativePath('attachments.location', result.file)
+		result.file.exists()
+		CONTENT.size() == result.file.size()
+	}
+
+	def testCreateAttachmentFile_empty_data() {
+		when:
+		def result = appResourceService.createAttachmentFile(2L, null,'txt', null)
+
+		then:
+		result == null
+	}
+
+	def testCreateAttachmentFile_file() {
+		when:
+		File origFile = new File('src/integration-test/resources/grails_logo.jpg')
+		def data = FileUtils.readFileToByteArray(origFile)
+		File tmpFile = appResourceService.createTempFile('grails_logo.jpg', data)
+		tmpFile.deleteOnExit()
+		def result = appResourceService.createAttachmentFile(2L, null,'jpg', tmpFile)
+
+		then:
+		result
+		result.location
+		result.location.size() > 3
+		result.location.endsWith(".jpg")
+		result.file
+		tmpFile.name+"_2.jpg" == result.file.name
+		result.location == appResourceService.getRelativePath('attachments.location', result.file)
+		result.file.exists()
+
+		cleanup:
+		result.file.delete()
+		tmpFile.delete()
+	}
+
+	def testCreateTempFile_empty() {
+		when:
+		def file = appResourceService.createTempFile("hello.txt", null)
+
+		then:
+		file
+		file.name.startsWith('hello')
+		file.name.endsWith('txt')
+		0 == file.size()
+		file.name == appResourceService.getRelativeTempPath(file)
+	}
+
+	def testCreateTempFile_string() {
+		when:
+		def file = appResourceService.createTempFile("hello.txt", 'hello, world!')
+
+		then:
+		file
+		file.name.startsWith('hello')
+		file.name.endsWith('txt')
+		13 == file.size()
+		file.name == appResourceService.getRelativeTempPath(file)
+	}
+
+	def testCreateTempFile_bytes() {
+		when:
+		def bytes = 'hello, world!'.getBytes()
+		def file = appResourceService.createTempFile("hello.txt", bytes)
+
+		then:
+		file
+		file.name.startsWith('hello')
+		file.name.endsWith('txt')
+		bytes.size() == file.size()
+		file.name == appResourceService.getRelativeTempPath(file)
+	}
+
+	def testDeleteTempUploadedFiles() {
+		when:
+		def file1 = appResourceService.createTempFile('file1.txt', 'hello, world!')
+		def file2 = appResourceService.createTempFile('file2.txt', 'goodbye cruel world.')
+
+		then:
+		file1.exists()
+		file2.exists()
+
+		when:
+		appResourceService.deleteTempUploadedFiles(
+			"[ {'tempFilename':'${file1.name}','originalFilename':'file1.txt','extension':'txt','filesQueued':'0'},"
+			+ "{'tempFilename':'${file2.name}','originalFilename':'file2.txt','extension':'txt','filesQueued':'0'}]"
+		)
+
+		then:
+		!file1.exists()
+		!file2.exists()
+	}
+
+	def testGetRootLocation1() {
+		when:
+		def dir = appResourceService.rootLocation
+
+		then:
+		dir != null
+		//File base = new File('target/resources/virgin-2/')
+		// This next line will fail if you change nine.attachments.directory in Config.groovy OR the test data for id=2
+		dir.path.endsWith(new File('/rootLocation').path)
+		dir.exists()
+		dir.isDirectory()
+		dir.canWrite()
+	}
+
+	def testGetTempDir() {
+		when:
+		def dir = appResourceService.getLocation('tempDir')
+
+		then:
+		dir
+		dir.absolutePath.endsWith(new File('/9ci-app/temp').path)
+		!dir.absolutePath.startsWith(appResourceService.rootLocation.absolutePath)
+		dir.exists()
+		dir.isDirectory()
+		//assertEquals true, dir.isAbsolute()
+		dir.canWrite()
+	}
+	def testGetRootLocation2() {
+		when:
+		File root = appResourceService.rootLocation
+		println "root location is ${root.absolutePath}"
+
+		then:
+		root
+		root.exists()
+		root.isDirectory()
+	}
+
+	def testGetLocation_absolute_scripts() {
+		when:
+		List scripts = appResourceService.scripts
+
+		then:
+		scripts[0].exists()
+		scripts[0].isDirectory()
+	}
+
+	def testGetLocation_absolute_tempDir() {
+		when:
+		File temp = appResourceService.getLocation('tempDir')
+
+		then:
+		temp.exists()
+		temp.isDirectory()
+	}
+
+	@spock.lang.Ignore
+	def testGetLocation_relative_checkImages() {
+		when:
+		File checkImages = appResourceService.getLocation('checkImage.location')
+		println "checkImageDir is ${checkImages.absolutePath}"
+
+		then:
+		checkImages.exists()
+		checkImages.isDirectory()
+	}
+
+	def test_getAttachmentsMonthDirectory() {
+		when:
+		def dir = appResourceService.getMonthDirectory('attachments.location')
+
+		then:
+		dir != null
+
+		when:
+		// This next line will fail if you change nine.attachments.directory in Config.groovy OR the test data for id=2
+		def datePart = new Date().format('yyyy-MM')
+
+		then:
+		dir.path.endsWith(new File("/attachments/${datePart}").path)
+		dir.exists()
+		dir.isDirectory()
+		dir.canWrite()
+	}
+
+	def test_getRelativeTempPath() {
+		when:
+		def dir = appResourceService.getLocation('tempDir')
+
+		then:
+		'blah' == appResourceService.getRelativeTempPath(new File(dir, 'blah'))
+	}
+
+	def testGetRelativePath() {
+		when:
+		def tmp = appResourceService.getLocation('tempDir')
+		def file = new File(tmp, 'blahBlah')
+
+		then:
+		appResourceService.getRelativePath('tempDir', file) == 'blahBlah'
+	}
+
+	def testMergeClientValues_emptyMap() {
+		when:
+		def result = appResourceService.mergeClientValues()
+
+		then:
+		result.tenantId==1
+		result.tenantSubDomain=='testTenant'
+		result.size() == 2
+	}
+
+	def testMergeClientValues_fullMap_noUnexpectedCollisions() {
+		when:
+		Map args = [tenantId:7, name:'blah', num: 'seventy', numbers:['one', 'two', 'three']]
+		def result = appResourceService.mergeClientValues(args)
+
+		then:
+		result.tenantId == 7
+
+		when:
+		result.tenantId = 2 // Should not transfer back to args
+
+		then:
+		args.tenantId == 7
+		result.tenantSubDomain == 'testTenant'
+		args.size() == 4
+		result.size() == 5
+		result.numbers[1] == 'two'
+		args.numbers[1] == 'two'
+
+		when:
+		result.numbers[1] = 'blah' // Replaces a deep value.
+
+		then:
+		args.numbers[1] == 'blah' // Bad, but expected.
+		args.num == 'seventy'
+
+		when:
+		result.num = 'seven' // Should be a shallow replacement, not passed back to args.
+
+		then:
+		args.num == 'seventy' // Should be a the same as before.
+	}
+}
