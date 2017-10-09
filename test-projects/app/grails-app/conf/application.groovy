e9939f4fb4e1b9f9bdd993b7ff0e1c9fcadcefc1
sudhir nimavat <sudhir@nimavat.me>
2017-10-09 18:50:34 +0530

    cleanup

--- test-projects/app/grails-app/conf/application.groovy
+++ test-projects/app/grails-app/conf/application.groovy
@@ -5,9 +5,6 @@ nine {
 		views.location = "views"
 
 		rootLocation = { args ->
-			// AppResourcesService will not create rootLocation directory or an absolute path.  This is in target, so we need something extra.
-			// For development we want a persistent non-SCM location common to all components but specific to a customer so we can
-			// have customer data or common external files.
 			File file = new File("test-projects/app/root-location")
 			if (!file.exists()) {
 				println "Creating rootLocation ${file.canonicalPath} for testing purposes."
